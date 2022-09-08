9c6d76c95b13f34d6ca3e2ecd53c61d871795b95
Guilherme Barbosa <guilherme.barbosa@tatic.net>
2022-09-08 14:03:56 -0300

    feat: algorithms for CountOddNumbersInRange and InorderTreeLevelTraversal

---
+++ src/main/java/io/gdfbarbosa/algorithms/trees/PreorderTreeTraversal.java
@@ -0,0 +1,24 @@
+package io.gdfbarbosa.algorithms.trees;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * <a href="https://leetcode.com/problems/binary-tree-preorder-traversal/">144. Binary Tree Preorder Traversal</a>
+ */
+public class PreorderTreeTraversal {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        preorderTraversalHelper(root, ans);
+        return ans;
+    }
+
+    public void preorderTraversalHelper(TreeNode node, List<Integer> ans) {
+        if (node == null) {
+            return;
+        }
+        ans.add(node.val);
+        preorderTraversalHelper(node.left, ans);
+        preorderTraversalHelper(node.right, ans);
+    }
+}
