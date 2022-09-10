b3f47af7577612a369eac6fde92ac427e0fed1f9
Guilherme Barbosa <guilherme.barbosa@tatic.net>
2022-09-10 13:14:51 -0300

    fix: rename file for Pow(x,n)

--- src/test/java/io/gdfbarbosa/algorithms/math/PowXNTest.java
+++ src/test/java/io/gdfbarbosa/algorithms/math/PowXNTest.java
@@ -5,8 +5,14 @@ import org.junit.Test;
 
 public class PowXNTest {
     @Test
-    public void test() {
+    public void test1() {
         PowXN math = new PowXN();
         Assert.assertEquals(27.0, math.myPow(3, 3), 0);
+    }
+
+    @Test
+    public void test2() {
+        PowXN math = new PowXN();
+        Assert.assertEquals(0.25, math.myPow(2, -2), 0);
     }
 }
