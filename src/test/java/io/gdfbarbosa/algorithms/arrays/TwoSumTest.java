50cc696969703e8f30c77588cb6c644287c958de
Guilherme Barbosa <guilherme.barbosa@tatic.net>
2022-09-11 08:29:24 -0300

    fix: algorithm for TwoSum

--- src/test/java/io/gdfbarbosa/algorithms/arrays/TwoSumTest.java
+++ src/test/java/io/gdfbarbosa/algorithms/arrays/TwoSumTest.java
@@ -12,6 +12,6 @@ public class TwoSumTest {
 
     @Test
     public void test2() {
-        Assert.assertArrayEquals(new int[]{0,1}, twoSum.twoSum(new int[]{2,7,11,15}, 9));
+        Assert.assertArrayEquals(new int[]{1,2}, twoSum.twoSum(new int[]{3,2,4}, 6));
     }
 }
