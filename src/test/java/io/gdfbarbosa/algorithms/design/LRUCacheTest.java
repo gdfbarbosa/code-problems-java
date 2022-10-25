efc00474aa48e285303ec93a8866fe126692f44a
Guilherme Barbosa <guilherme.barbosa@tatic.net>
2022-10-25 17:22:42 -0300

    feat: Algorithm for LRU Cache design problem

---
+++ src/test/java/io/gdfbarbosa/algorithms/design/LRUCacheTest.java
@@ -0,0 +1,22 @@
+package io.gdfbarbosa.algorithms.design;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class LRUCacheTest {
+    @Test
+    public void test() {
+        LRUCache lRUCache = new LRUCache(2);
+        lRUCache.put(1, 1); // cache is {1=1}
+        lRUCache.put(2, 2); // cache is {1=1, 2=2}
+        assertEquals(Integer.valueOf(1), lRUCache.get(1));    // return 1
+        lRUCache.put(3, 3); // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
+        assertEquals(Integer.valueOf(-1), lRUCache.get(2));    // returns -1 (not found)
+        lRUCache.put(4, 4); // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
+        assertEquals(Integer.valueOf(-1), lRUCache.get(1));    // return -1 (not found)
+        assertEquals(Integer.valueOf(3), lRUCache.get(3));    // return 3
+        assertEquals(Integer.valueOf(4), lRUCache.get(4));    // return 4
+    }
+
+}
